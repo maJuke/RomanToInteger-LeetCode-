084e8929f65d32ef684241cbeb8280154357c456
maJuke <80636069+maJuke@users.noreply.github.com>
2022-09-06 12:22:12 +1000

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        String s = "XIV";
+        String s = "XIV"; // example String
         System.out.println(Translate.romanToInt(s));
     }
 }
